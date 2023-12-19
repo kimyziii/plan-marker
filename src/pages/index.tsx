81a7a552e91210a59680c5c7f489d662303997bc
kimyziii <yeaji105@naver.com>
2023-12-20 02:26:55 +0900

    feat: 카카오 지도 연동 및 프로젝트 버전 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,5 @@
-import Link from '../../node_modules/next/link'
+import Map from '../components/Map'
 
 export default function Home() {
-  return (
-    <>
-      <div>
-        <div>
-          <Link href='/plan/new'>경로 생성 페이지</Link>
-        </div>
-        <div>
-          <Link href='/plan/list'>경로 리스트 페이지</Link>
-        </div>
-        <div>
-          <Link href='/plan/1'>경로 상세 페이지</Link>
-        </div>
-        <div>
-          <Link href='/plan/1/edit'>경로 수정 페이지</Link>
-        </div>
-        <div>
-          <Link href='/login'>로그인</Link>
-        </div>
-        <div>
-          <Link href='/user/1'>마이페이지</Link>
-        </div>
-        <div>
-          <Link href='/user/1/likes'>찜한 경로</Link>
-        </div>
-
-        <div>
-          <h2>경로 리스트 컴포넌트</h2>
-        </div>
-      </div>
-    </>
-  )
+  return <Map />
 }
