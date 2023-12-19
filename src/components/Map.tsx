d76959a90f788613158a89f4979e1b41c6180c8e
kimyziii <yeaji105@naver.com>
2023-12-20 02:49:43 +0900

    feat: 지도 높이 설정 변경 (navbar 높이 제외)

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,5 +1,6 @@
 /*global kakao*/
 import Script from 'next/script'
+import { useEffect } from 'react'
 
 declare global {
   interface Window {
@@ -7,7 +8,25 @@ declare global {
   }
 }
 
+/**
+ * navbar의 높이만큼 뺀 높이로 지도 높이 설정
+ */
+const setRootHeight = () => {
+  const root = document.documentElement
+  root.style.setProperty('--window-height', `${window.innerHeight - 50}px`)
+}
+
 export default function Map() {
+  useEffect(() => {
+    setRootHeight()
+    window.addEventListener('resize', setRootHeight)
+
+    // 컴포넌트 언마운트 될 때 제거
+    return () => {
+      window.removeEventListener('resize', setRootHeight)
+    }
+  }, [])
+
   function loadKakaoMap() {
     window.kakao.maps.load(function () {
       const container = document.getElementById('map')
@@ -27,7 +46,7 @@ export default function Map() {
         src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_CLIENT}&autoload=false`}
         onReady={loadKakaoMap}
       />
-      <div id='map' className='w-full h-screen'></div>
+      <div id='map' className='w-full map'></div>
     </>
   )
 }
