faa1df0c9c496352040193cdd2b1ed241b978295
kimyziii <yeaji105@naver.com>
2024-01-05 22:51:20 +0900

    feat: 경로 상세 페이지 및 데이터 불러오기

--- src/pages/api/plan.ts
+++ src/pages/api/plan.ts
@@ -1,18 +1,50 @@
 import { PrismaClient } from '@prisma/client'
 import { NextApiRequest, NextApiResponse } from 'next'
 
-const prisma = new PrismaClient()
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>,
 ) {
+  const prisma = new PrismaClient()
+  // POST
   if (req.method === 'POST') {
     const data = req.body
 
     const createdPlan = await prisma.plan.create({
       data,
     })
+
+    return res.status(200).json({ ok: true, result: createdPlan })
   }
-  return res.status(200).json({ ok: true })
+
+  // GET
+  else if (req.method === 'GET') {
+    console.log(`get method call`)
+    const { uId, pId } = req.query
+
+    let plans
+
+    if (uId) {
+      const strUId = uId.toString()
+      plans = await prisma.plan.findMany({
+        where: {
+          createdById: strUId,
+        },
+      })
+    } else if (pId) {
+      const strPId = pId.toString()
+      plans = await prisma.plan.findMany({
+        include: {
+          user: true,
+        },
+        where: {
+          id: strPId,
+        },
+      })
+    }
+
+    await prisma.$disconnect()
+
+    return res.status(200).json({ plans })
+  }
 }
--- src/pages/plan/[id]/index.tsx
+++ src/pages/plan/[id]/index.tsx
@@ -1,11 +1,168 @@
+import { mapState } from '@/atom'
+import { Map } from '@/components/Map'
+import axios from 'axios'
+import Image from 'next/image'
 import { useRouter } from 'next/router'
+import { useQuery } from 'react-query'
+import { useRecoilValue } from 'recoil'
 
 export default function PlanDetailPage() {
-  const router = useRouter()
-  const { id } = router.query
+  const { id } = useRouter().query
+  const map = useRecoilValue(mapState)
+
+  const { data } = useQuery(
+    `plan-${id}`,
+    async () => {
+      const { data } = await axios(`/api/plan?pId=${id}`)
+      const markerData = JSON.parse(data.plans[0].data)
+
+      return { plan: data.plans[0], markerData: markerData }
+    },
+    {
+      refetchOnWindowFocus: false,
+      enabled: !!id,
+    },
+  )
+
+  if (data && map) {
+    getMap()
+  }
+
+  function getMap() {
+    // 마커 작업
+    var imageSrc = '/icons/default-marker.svg'
+    var imageSize = new window.kakao.maps.Size(30, 35)
+    var markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize)
+
+    var bounds = new window.kakao.maps.LatLngBounds()
+    data.markerData.forEach((data) => {
+      const latlng = new window.kakao.maps.LatLng(data.y, data.x)
+
+      var marker = new window.kakao.maps.Marker({
+        map: map,
+        position: latlng,
+        title: data.place_name,
+        image: markerImage,
+      })
+
+      var content =
+        '<div className="customoverlay" style="padding: 3px 8px; background-color: #EEF6FF; border-radius: 14px; border: 1px solid #EDF2FD; color: #2E5BDC; font-size: small;">' +
+        `    <span className="title">${data.place_name}</span>` +
+        '</div>'
+
+      var customOverlay = new window.kakao.maps.CustomOverlay({
+        map: map,
+        position: latlng,
+        content: content,
+        xAnchor: 0.5,
+        yAnchor: 0,
+      })
+
+      window.kakao.maps.event.addListener(marker, 'mouseover', function () {
+        marker.setZIndex(100)
+        customOverlay.setZIndex(100)
+      })
+
+      window.kakao.maps.event.addListener(marker, 'mouseout', function () {
+        marker.setZIndex(1)
+        customOverlay.setZIndex(1)
+      })
+
+      bounds.extend(latlng)
+    })
+
+    map.setBounds(bounds)
+    console.log(data.markerData)
+  }
+
   return (
-    <div>
-      <h1>플랜 상세 페이지: {id}</h1>
+    <div className='w-[90%] mx-auto'>
+      {data && (
+        <div className='w-full flex flex-col gap-4'>
+          {/* 제목, 작성자, 작성일자 */}
+          <div className='w-full mx-4 my-5 h-[6vh]'>
+            <div className='flex flex-col gap-2'>
+              <div className='text-2xl font-bold leading-7 text-gray-900'>
+                {data.plan?.title}
+              </div>
+              <div className='flex flex-row items-center text-sm text-gray-500 gap-4'>
+                <div className='flex flex-row items-center gap-2'>
+                  <Image
+                    src='/icons/user.svg'
+                    alt='생성일자'
+                    width='18'
+                    height='18'
+                  />
+                  <span>{data.plan?.user.name}</span>
+                </div>
+                <div className='flex flex-row items-center gap-2'>
+                  <Image
+                    src='/icons/calendar.svg'
+                    alt='생성일자'
+                    width='18'
+                    height='18'
+                  />
+                  <span>
+                    {new Date(data.plan?.createdAt).toLocaleString('ko-KR')}
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='flex gap-4'>
+            <div className='w-1/3 overflow-hidden'>
+              {/* 지도 */}
+              <div className='w-full mx-4 px-4 h-[75vh]' id='map'>
+                <Map type='full' />
+              </div>
+            </div>
+
+            {/* 장소 정보 */}
+            <div className='w-2/3'>
+              <table className='w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 mb-3'>
+                <thead className='text-xs text-gray-700 bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
+                  <tr>
+                    <th scope='col' className='px-3 py-3 text-center'>
+                      No
+                    </th>
+                    <th scope='col' className='px-3 py-3 text-center'>
+                      시간
+                    </th>
+                    <th scope='col' className='px-3 py-3'>
+                      장소명
+                    </th>
+                    <th scope='col' className='px-3 py-3'>
+                      메모
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {data.markerData.map((data, index) => (
+                    <tr
+                      key={data.id}
+                      className='bg-white border-b dark:bg-gray-800 dark:border-gray-700'
+                    >
+                      <th
+                        scope='row'
+                        className='w-[10%] px-3 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white text-center'
+                      >
+                        {index + 1}
+                      </th>
+                      <td className='w-[10%] whitespace-nowrap text-center px-3 py-3'>
+                        {data.hour}:{data.minute}
+                      </td>
+                      <td className='w-[30%] px-2 py-2'>{data.place_name}</td>
+                      <td className='w-[50%] px-2 py-2 min-h-12'>
+                        {data.memo}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
