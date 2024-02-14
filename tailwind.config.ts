5689aa86deb6efebc052e82aae280f04ca90d53f
kimyziii <yeaji105@naver.com>
2024-02-14 15:48:52 +0900

    design: screen size config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,11 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      mobile: '320px',
+      md: '769px',
+      lg: '1024px',
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
